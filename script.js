9a2e1a6f1ad0a13798751292d846da600853792d
Suman3987 <101932347+Suman3987@users.noreply.github.com>
2022-11-25 20:20:58 +0000

    css update

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ myText.addEventListener("input", function () {
   result.textContent = textLength + "/" + limit;
 
   if (textLength > limit) {
-    myText.style.borderColor = "#ff2851";
-    result.style.color = "#ff2851";
+    myText.style.borderColor = "blue";
+    result.style.color = "blue";
   } else {
     myText.style.borderColor = "#b2b2b2";
     result.style.color = "#737373";
